315e97addbf31d6e8f9c9fdfcf22cc93c10a8b66
leosimonato <47404251+leosimonato@users.noreply.github.com>
2020-05-25 19:41:04 -0300

    Criação de botões e métodos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'poc-cookie2';
+   title = 'poc-cookie';
+
+   nomeCookie = "testString";
+   testString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+   constructor(private cookieService: CookieService) {
+   }
+
+   addCookie() {
+      this.cookieService.set(this.nomeCookie, this.testString);
+   }
+
+   verifyCookie() {
+      const existe = this.cookieService.check(this.nomeCookie);
+      if (existe) {
+         alert(`Cookie existe`);
+      } else {
+         alert(`Cookie inexistente`);
+      }
+   }
+
+   deleteCookie() {
+      this.cookieService.delete(this.nomeCookie);
+   }
 }
